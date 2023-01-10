9d01ba0b0684f93bfcd819bdee8a167ab78e1629
cibrown94 <cianibrown94@aol.com>
2023-01-09 16:16:04 -0800

    second commit

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -9,7 +9,7 @@ class ApartmentsController < ApplicationController
         apartment = Apartment.create(apartment_params)
         if apartment.valid?
         render json: apartment
-        else render json: apartment.errors
+        else render json: apartment.errors, status:422
         end
       end
       
--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe "Apartments", type: :request do
     end
   end
 
-  describe "POST /create" do
+  describe 'POST /create' do
     it "create an apartment" do
       user = User.where(email: 'test@test.test').first_or_create(password: '12345678', password_confirmation: '12345678')
       apartment_params = {
@@ -41,13 +41,28 @@ RSpec.describe "Apartments", type: :request do
         pets:"cats only",
         image:"exampleimage.com", 
         user_id: user.id
-      }
-    }
+        }}
+        post '/apartments', params: apartment_params
+        expect(response).to have_http_status(200)
+        apartment = Apartment.first
+        expect(apartment.street).to eq "123 Main St"
+      end
+    
 
-    post '/apartments', params: apartment_params
-    expect(response).to have_http_status(200)
-    apartment = Apartment.first
-    expect(apartment.street).to eq "123 Main St"
+    it "doesn't create a apartment without a street, city, state, manager, email, price, bedrooms, bathrooms, pets, image, user id" do
+      user = User.where(email: 'test@test.test').first_or_create(password: '12345678', password_confirmation: '12345678')
+      apartment_params = {
+        apartment: {
+        
+        user_id: user.id
+      }}
+      post '/apartments', params: apartment_params
+      expect(response.status).to eq(422)
+      json = JSON.parse(response.body)
+      expect(json['street']).to include "can't be blank"
+    end
   end
 end
-end
+
+
+  
